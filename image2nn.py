9ea2953c0791ed58d6f5ddf20ffa77757fff97a0
Alexander Sayapin <alstutor@gmail.com>
2020-03-02 11:19:56 +0700

    Improve input data check for NPArrayToTensor

--- image2nn.py
+++ image2nn.py
@@ -14,6 +14,12 @@ class DataToTensor(ABCMeta):
 
 class NPArrayToTensor(DataToTensor):
     def __call__(cls, data: np.array) -> torch.Tensor:
+        if not data:
+            raise ValueError('No data array to convert to tensor')
+            
+        if len(data.shape) != 4:
+            raise ValueError('The size of the array you are trying to convert is incorrect: {}, instead of 4'.format(data.shape))
+        
         tensor = torch.from_numpy(data)
         tensor = tensor[np.newaxis, :]
         tensor = tensor.permute(0, 3, 2, 1)
@@ -27,15 +33,17 @@ class ImageArrayGetter(ABCMeta):
 
 
 class CV2CamImageArrayGetter(ImageArrayGetter):
-    def __init__(cls, camera):
-        super().__init__(camera)
+    def __init__(cls, resizer: callable, camera: any):
+        super().__init__(camera, resizer)
         cls._camera = camera
+        cls._resizer = resizer
 
-    def __call__(cls) -> np.array:
+    def __call__(cls, image_size: Tuple[int, int]) -> np.array:
         ret, image = cls._camera.read()
         if not ret:
             raise RuntimeError("Cannot get mage from the camera")
 
+        image = cls._resizer(image, image_size)
         return image
 
 
