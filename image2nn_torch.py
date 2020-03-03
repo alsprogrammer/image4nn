50cbf29973ba159d83794d7ea034856f17ee9c3f
Alexander Sayapin <alstutor@gmail.com>
2020-03-03 10:25:24 +0700

    Move tensor handling

--- image2nn_torch.py
+++ image2nn_torch.py
@@ -7,11 +7,20 @@ import torch
 class DataToTensor(ABCMeta):
     @abstractmethod
     def __call__(cls, data: any) -> torch.Tensor:
-        raise NotImplementedError('The method is not implemented')
+        pass
 
 
 class NPArrayToTensor(DataToTensor):
     def __call__(cls, data: np.array) -> torch.Tensor:
+        if not data:
+            raise ValueError('No data array to convert to tensor')
+            
+        if not isinstance(data, np.array):
+            raise ValueError('The data you are passing is not a numpy array')
+            
+        if len(data.shape) != 4:
+            raise ValueError('The size of the array you are trying to convert is incorrect: {}, instead of 4'.format(data.shape))
+        
         tensor = torch.from_numpy(data)
         tensor = tensor[np.newaxis, :]
         tensor = tensor.permute(0, 3, 2, 1)
