19ee885dcf296c51cfc4c81985563f5d67c7109e
Alexander Sayapin <alstutor@gmail.com>
2020-03-03 12:58:41 +0700

    Fix parameters list

--- image2nn_picamera.py
+++ image2nn_picamera.py
@@ -7,12 +7,19 @@ from time import sleep
 
 class RaspiCamImageArrayGetter(ImageArrayGetter):
     def __init__(cls):
+    def __init__(cls, resizer: callable):
+        if not resizer:
+            raise ValueError('The resizer cannot be empty')
+            
+        super().__init__(resizer)
+        cls._resizer = resizer        
         cls._camera = PiCamera()
         cls._rawCapture = PiRGBArray(cls._camera)
         sleep(0.1)
 
-    def __call__(cls) -> np.array:
+    def __call__(cls, image_size: Tuple[int, int]) -> np.array:
         cls._camera.capture(cls._rawCapture, format="bgr")
         image = rawCapture.array
+        image = cls._resizer(image, image_size)
 
         return image
