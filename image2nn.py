94de19e7bc1fe650135531c82db61797d2152a20
Alexander Sayapin <alstutor@gmail.com>
2020-03-03 10:24:41 +0700

    Remove NPArrayToTensor

--- image2nn.py
+++ image2nn.py
@@ -6,29 +6,6 @@ import torch
 import cv2
 
 
-class DataToTensor(ABCMeta):
-    @abstractmethod
-    def __call__(cls, data: any) -> torch.Tensor:
-        pass
-
-
-class NPArrayToTensor(DataToTensor):
-    def __call__(cls, data: np.array) -> torch.Tensor:
-        if not data:
-            raise ValueError('No data array to convert to tensor')
-            
-        if not isinstance(data, np.array):
-            raise ValueError('The data you are passing is not a numpy array')
-            
-        if len(data.shape) != 4:
-            raise ValueError('The size of the array you are trying to convert is incorrect: {}, instead of 4'.format(data.shape))
-        
-        tensor = torch.from_numpy(data)
-        tensor = tensor[np.newaxis, :]
-        tensor = tensor.permute(0, 3, 2, 1)
-        return tensor
-
-
 class ImageArrayGetter(ABCMeta):
     @abstractmethod
     def __call__(cls) -> np.array:
